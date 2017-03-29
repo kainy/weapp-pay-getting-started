f76e3c54d8a567495e47d24e498f0833d6f88f32
KainyGuo <gt@kainy.cn>
2017-03-29 10:28:55 +0800

    模板消息“商品名称”颜色及链接到打赏页。

--- order.js
+++ order.js
@@ -66,6 +66,7 @@ class Order extends AV.Object {
       data: {
         "keyword1": {
           "value": '小程序打赏',
+          "color": "#1aad19",
         },
         "keyword2": {
           "value": `${this.amount / 100} 元`,
@@ -78,6 +79,7 @@ class Order extends AV.Object {
         }
       },
       emphasis_keyword: 'keyword1.DATA',
+      page: "pages/my-pay/pay",
     };
     console.log('send notice: ', data);
     return getAccessToken().then(accessToken =>
