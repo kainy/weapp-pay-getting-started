71544222ab52c5647c7b279070a35de8f97c49de
Kainy Guo <GT@Kainy.CN>
2017-08-27 19:10:01 +0800

    生成小程序码。

--- routes/minaCode.js
+++ routes/minaCode.js
@@ -5,17 +5,26 @@ const AV = require('leanengine')
 
 router.get('/minacode', function (req, res, next) {
   getAccessToken().then((accessToken) => {
-    // console.log(accessToken)
     const data = {
-      path: 'pages/index?query=1'
+      'page': req.query.page || 'pages/index/index',
+      'scene': req.query.scene || 'xxx'
     }
-    axios.post('https://api.weixin.qq.com/wxa/getwxacode', data, {
+    console.log(data)
+    axios.post('https://api.weixin.qq.com/wxa/getwxacodeunlimit', data, {
+    // axios.post('https://api.weixin.qq.com/wxa/getwxacode', data, {
       params: {
-        access_token: accessToken
-      }
-    }).then(({data}) => {
-      var bytes = [0xBE, 0xEF, 0xCA, 0xFE]
-      return new AV.File('minacode.png', bytes).save()
+        access_token: accessToken,
+        dataType: 'JSON'
+      },
+      responseType: 'arraybuffer'
+    }).then((data) => {
+      var imageFile = new AV.File('file-qrcode', data.data)
+      return imageFile.save()
+      // var bytes = [0xBE, 0xEF, 0xCA, 0xFE]
+      // return new AV.File('minacode.png', bytes).save()
+    }).then(result => {
+      // console.log(result)
+      res.send(result)
     })
   })
 })
